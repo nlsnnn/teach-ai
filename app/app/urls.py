fa7a345fe342d40436b1ea96ca87fd3c5a065332
nlsnnn <egor.larrr@gmail.com>
2025-09-22 13:04:26 +0300

    WIP: add users views

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("auth/", include(("users.urls", "users"), namespace="users")),
+    path("lesson/", include(("lesson_plans.urls", "lesson_plans"), namespace="lessons")),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView  # noqa: F401
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import FormView
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+class UserRegisterView(FormView):
+    form_class = CustomUserCreationForm
+    template_name = ""
+    # success_url = ""
+
+
+class UserLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'users/login.html'
+    # success_url = ""
+
